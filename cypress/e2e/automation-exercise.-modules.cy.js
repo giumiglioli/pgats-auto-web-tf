ebacfdc9ee0e629299a6a70d11b7a05eb8fbb061
Giuliana Miglioli <giuliana.miglioli@gmail.com>
2025-11-02 17:18:24 -0300

    Adding last tests without cleaning

--- cypress/e2e/automation-exercise.-modules.cy.js
+++ cypress/e2e/automation-exercise.-modules.cy.js
@@ -13,11 +13,11 @@ import register from '../modules/register/index.js';
 import contact from '../modules/contact';
 
 
-describe('Automation Exercise Testing that need Register/Logged In', () => {
+describe('Automation Exercise Testing that need Register/Logged In page', () => {
 
     beforeEach(() => {
         //Runs before each test in the block
-        //Define viewport and visit the website
+        //Define viewport and visit the website and click on the login link
         cy.viewport(`ipad-2`);
         cy.visit('https://automationexercise.com/');
         menu.navigateToLoginPage();
@@ -96,9 +96,85 @@ describe('Automation Exercise Testing that need Register/Logged In', () => {
     
     }); // it
 
+    it(' TEST 15 - PLACE ORDER: REGISTER BEFORE CHECKOUT', () => {
+        //First screen for register        
+        login.FillPreRegisterUser();
+        //Second screen for register - Enter Account Information
+        register.FillRegisterUserComplete();
+
+        //Assertions - same as Test 1
+        cy.url().should('include','account_created'); //validate the url text
+        cy.contains('b', 'Account Created!'); //check if the text is displayed on page 
+        cy.get('h2[data-qa="account-created"]').should('have.text', 'Account Created!');//validate the text on H2 tag with data-qa attribute
+
+         // 1. Add product in cart
+        cy.visit('https://automationexercise.com/products');
+        cy.get('.features_items .product-image-wrapper').first().trigger('mouseover');
+        cy.get('.features_items .product-image-wrapper').first().contains('Add to cart').click();
+
+         // 2. Go to cart page and verify that product is visible in cart
+        cy.get('u').contains('View Cart').click();
+        cy.url().should('include', '/view_cart');
+        cy.get('.cart_info').should('be.visible');
+
+        // 3. Click in "Proceed To Checkout"
+        cy.contains('Proceed To Checkout').click();
+        //Check if page of review your order is displayed
+        cy.contains('h2', 'Review Your Order').should('be.visible'); // ajuste conforme o texto real
+
+        //4. Verify Address Details on the Review Your Order page
+        // Verify delivery address details
+        cy.get('#address_delivery').should('be.visible');
+        //selector: #address_delivery > .address_title > .page-subheading picked up in cypress
+        cy.get('#address_delivery > .address_title > .page-subheading').should('contain.text', 'Your delivery address'); //verifying the title of delivery address section
+        cy.get('#address_delivery > .address_firstname').should('contain.text', login.name);
+        cy.get('#address_delivery').should('contain.text', register.address);
+        cy.get('#address_delivery').should('contain.text', register.city);
+        cy.get('#address_delivery').should('contain.text', register.state);
+        cy.get('#address_delivery').should('contain.text', register.country);
+        cy.get('#address_delivery').should('contain.text', register.zipcode);
+
+        // Verify the invoice address - which is the same as delivery address
+        cy.get('#address_invoice').should('be.visible');
+        cy.get('#address_invoice  > .address_title > .page-subheading').should('contain.text', 'Your billing address'); //verifying the title of delivery address section
+        cy.get('#address_invoice  > .address_firstname').should('contain.text', login.name);
+        cy.get('#address_invoice').should('contain.text', register.address);
+        cy.get('#address_invoice').should('contain.text', register.city);
+        cy.get('#address_invoice').should('contain.text', register.state);
+        cy.get('#address_invoice').should('contain.text', register.country);
+        cy.get('#address_invoice').should('contain.text', register.zipcode);
+
+        //Enter description in comment text area and click 'Place Order'
+        cy.get('textarea[name="message"]').type('Please deliver as soon as possible.');
+        cy.contains('Place Order').click();
+
+        //5. Enter payment details: Name on Card, Card Number, CVC, Expiration date
+        cy.get('.heading').should('contain.text', 'Payment');
+        // Aguarde overlays sumirem, se existirem
+        cy.get('.modal, .loader, .overlay', { timeout: 10000 }).should('not.exist');
+        // Role até o campo
+        cy.get('[data-qa="name-on-card"]').scrollIntoView().should('be.visible').and('not.be.disabled');
+        // Pequeno wait para garantir fim de animação (último recurso)
+        cy.wait(300);
+        cy.get('[data-qa="name-on-card"]').type('Test User', { force: true });
+        cy.get('[data-qa="card-number"]').type('4111111111111111', { force: true });
+        cy.get('[data-qa="cvc"]').type('123', { force: true });
+        cy.get('[data-qa="expiry-month"]').type('12', { force: true });
+        cy.get('[data-qa="expiry-year"]').type('2030', { force: true });
+
+        //6. Click 'Pay and Confirm Order' button
+        cy.get('[data-qa="pay-button"]').click();
+
+        // Assertion, success message 'Order Placed!'
+        cy.get('[data-qa="order-placed"] > b').should('contain.text', 'Order Placed!');
+        cy.contains("p", "Congratulations! Your order has been confirmed!").should("be.visible") 
+        cy.get('[data-qa="continue-button"]').should('contain.text', 'Continue');
+        
+    }); //it
+
 }); // describe
 
-describe('Automation Exercise Testing Without the need to be Logged In', () => {
+describe('Automation Exercise Testing Without the need to be Login page', () => {
 
     beforeEach(() => {
         //Runs before each test in the block
@@ -119,14 +195,78 @@ describe('Automation Exercise Testing Without the need to be Logged In', () => {
 
     it(' TEST 08 - VERIFY ALL PRODUCTS AND PRODUCT DETAILS', () => {
         
-        cy.get('i.fa-home').parent().should('contain.text', 'Home');
-        //Access Products page
-        cy.get('a[href="/products"]').click();
+        //1. Access Products page
+        cy.get('a[href="/products"]').click(); 
+        // 2. Verify if the products page were loaded 
+        cy.url().should('include', '/products');
+        cy.contains('h2', 'All Products').should('be.visible');
 
-        //Access Products page
+        // 3. The product list is visible
+        cy.get('.features_items').should('be.visible');
+
+        // 4. Check if there is at least one prodsuct on the list 
+        cy.get('.features_items').should('have.length.greaterThan', 0);
+
+        // 5. Click on the view product of the first product
+        cy.get('.features_items .product-image-wrapper').first().find('a').contains('View Product').click();
+
+        // 6. Check if the detail page were loaded correctly 
+        cy.url().should('include', '/product_details');
+        cy.get('.product-information').should('be.visible');
+
+        // 7. Verify that detail detail is visible: product name, category, price, availability, condition, brand
+        //The checks on last step of testing 
+        cy.get('.product-information h2').should('not.be.empty'); // Product Name
+        cy.get('.product-information p').should('contain.text', 'Category'); //Category
+        cy.get('.product-information span span').should('not.be.empty'); // Price
+        cy.get('.product-information p').should('contain.text', 'Availability'); // Availability
+        cy.get('.product-information p').should('contain.text', 'Condition'); // Condition
+        cy.get('.product-information p').should('contain.text', 'Brand'); //Brand
+
     }); // it
 
-    it(' TEST 09 - ADD PRODUCTS TO CART', () => {
-        //Access Products page
-    });
+    it(' TEST 09 - SEARCH PRODUCT', () => {
+
+        //1. Access Products page
+        cy.get('a[href="/products"]').click(); 
+        // 2. Verify if the products page were loaded 
+        cy.url().should('include', '/products');
+        cy.contains('h2', 'All Products').should('be.visible');
+
+        // 3. The product list is visible
+        cy.get('.features_items').should('be.visible');
+        // 4. Type product name in search input and click search button
+        //const productName = 'Sleeveless Dress';
+        cy.get('#search_product').type('Winter Top');
+        cy.get('#submit_search').click();
+
+        // 5. Verify if the searched products page is displayed
+        cy.contains('h2', 'Searched Products').should('be.visible');
+
+        // 6. Verify that all the products related to search are visible
+        cy.get('.features_items').should('be.visible');
+        cy.get('.features_items .product-image-wrapper').each(($el) => {
+            cy.wrap($el).find('.productinfo p').should('contain.text', 'Winter Top');
+        });
+
+    }); // it
+
+    it(' TEST 10 - VERIFY SUBSCRIPTION IN HOME PAGE', () => {
+        // 1. Scroll to bottom of page where 
+        cy.get('footer').scrollIntoView();;
+
+        // 2. Verify text SUBSCRIPTION
+        cy.contains('h2', 'Subscription').should('be.visible');
+
+        // 3. Enter email address in input and click arrow button
+        const randomEmail = getRandomEmail();
+        cy.get('#susbscribe_email').type(randomEmail); //There is a typo on page 'susbscribe_email' Needed help from copilot to figure it out.
+        cy.get('#subscribe').click();
+
+        // 4. Verify success alert message 'You have been successfully subscribed!' is visible
+        cy.get('.alert-success').should('be.visible');
+        cy.get('.alert-success').should('have.text', 'You have been successfully subscribed!');
+    }); //it
+
+
 }); // describe
--- cypress/modules/register/index.js
+++ cypress/modules/register/index.js
@@ -4,16 +4,30 @@ import Login from '../login';
 import { faker } from '@faker-js/faker';
 
 class Register{
+    //Properties to store user information 
+        address = '';
+        city = '';
+        state = '';
+        country = '';
+        zipcode = '';
+
+
+
     FillRegisterUserComplete(){
         const birthday = getRandomBirthday();
         const gender = getRandomGender();
 
+        this.address = faker.location.streetAddress();
+        this.city = faker.location.city();
+        this.state = faker.location.state();
+        this.country = 'United States';
+        this.zipcode = faker.location.zipCode();
+
         cy.get('input[type="radio"]').check(gender);
-        cy.get('input#password').type('QAgmteste1234'),{log: false};//to hide password in Cypress logs
-        //para comboboxes ou selects -> select
+        cy.get('input#password').type('QAgmteste1234', { log: false }); //to hide password in Cypress logs
         cy.get('select[data-qa=days]').select(birthday.day);
         cy.get('select[data-qa=months]').select(birthday.month);
-        cy.get('select[data-qa=years]').select(birthday.year); 
+        cy.get('select[data-qa=years]').select(birthday.year);
         //radio ou checkboxes -> check
         cy.get('input[type=checkbox]#newsletter').check();
         cy.get('input[type=checkbox]#optin').check();
@@ -21,18 +35,19 @@ class Register{
         cy.get('input#first_name').type(Login.name);
         cy.get('input#last_name').type(faker.person.lastName());
         cy.get('input#company').type(`GM ${faker.company.name()}`);
-        cy.get('input#address1').type(faker.location.streetAddress());
-        //cy.get('input#address2').type('Apto 101'); NO need to fill this information
-        cy.get('select#country').select('United States');
-        cy.get('input#state').type(faker.location.state());
-        cy.get('input#city').type(faker.location.city());
-        cy.get('[data-qa="zipcode"]').type(faker.location.zipCode());
+        cy.get('input#address1').type(this.address);
+        cy.get('select#country').select(this.country);
+        cy.get('input#state').type(this.state);
+        cy.get('input#city').type(this.city);
+        cy.get('[data-qa="zipcode"]').type(this.zipcode);
         cy.get('[data-qa="mobile_number"]').type(getRandomPhone());
 
-        //Act
+        
         cy.get('[data-qa="create-account"]').click();
-
     }
+
+
+    
 
     RegisterWithExistingEmail(name, email) {
         cy.get('[data-qa="signup-name"]').type(name);
